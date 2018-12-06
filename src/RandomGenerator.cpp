b5f0535d00725f012aaf34e1dcc58910e57f4b1c
mounirq <mounir.qat@gmail.com>
2018-12-06 16:39:59 +0100

    Pricer

---
+++ src/RandomGenerator.cpp
@@ -0,0 +1,16 @@
+#include "RandomGenerator.hpp"
+
+
+RandomGenerator::RandomGenerator(){
+}
+
+
+RandomGenerator::RandomGenerator(const RandomGenerator * randomGenerator){
+    
+}
+
+RandomGenerator::~RandomGenerator(){}
+
+void RandomGenerator::fillMatrix(PnlMat *matrix, int nbOfRows, int nbOfCol) {
+
+}
